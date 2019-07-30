3b0749d6e0498404b22b6964ff502d951fa76392
Jonni <bisslad@gmail.com>
2019-07-30 11:28:03 +0100

    Completed test set for anonymous users

--- integration/tests/anonymous.js
+++ integration/tests/anonymous.js
@@ -7,10 +7,45 @@ context('Preliminary', () => {
 
 
   it('Loads the homepage', () => {
-    cy.visit('http://pages.bline.ie');
+    cy.visit('/');
     cy.url().should('match', /pages.bline.ie/);
   })
 
+  it('Loads the login page', () => {
+    cy.visit('/user/login');
+    cy.url().should('contain', "user/login");
+  })
 
+  it('Can access published content', () => {
+    cy.visit(Cypress.env("cyTestArticleUrl"));
+    cy.get('.h1 > span').should("contain", "Cypress");
+  })
 
+  it('Cannot access unpublished content', () => {
+
+  cy.request({ method: 'GET', url: Cypress.env("cyTestArticle2Url"), failOnStatusCode: false })
+    .then((response) => {
+      expect(response.status).to.eq(403);
+    })
+  })
+
+  it('Can access published paragraph content', () => {
+    cy.visit('node/4');
+    cy.get('.paragraph--masthead');
+  })
+
+  it('Cannot access unpublished paragraph content', () => {
+    cy.request({ method: 'GET', url: 'node/14', failOnStatusCode: false })
+    .then((response) => {
+      expect(response.status).to.eq(403);
+    })
+  })
+
+  it('Cannot access the admin area', () => {
+    cy.request({ method: 'GET', url: 'admin', failOnStatusCode: false })
+    .then((response) => {
+      expect(response.status).to.eq(403);
+      cy.screenshot();
+    })
+  })
 })
